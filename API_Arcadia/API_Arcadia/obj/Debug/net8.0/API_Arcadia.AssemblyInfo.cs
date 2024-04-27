eb51d469dce478cac0a3345eaa78e36a11253b0c
LaconisFaucheux <nicolas.frotey@gmail.com>
2024-04-27 20:07:57 +0200

    install lib auth

--- API_Arcadia/API_Arcadia/obj/Debug/net8.0/API_Arcadia.AssemblyInfo.cs
+++ API_Arcadia/API_Arcadia/obj/Debug/net8.0/API_Arcadia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f79d6873341c20b00cbbc90c37639dfdb8dcf4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fd347f0b3eed2b70223eada0440baa9345a06e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
