2aa2db8c592c04d8f03c5d6d365c381a15ed216d
Nicolas Frotey <nicolas.frotey@shark-robotics.fr>
2024-05-17 08:36:00 +0200

    changement de PC

--- API_Arcadia/IdentityServerHost/obj/Debug/net8.0/IdentityServerHost.AssemblyInfo.cs
+++ API_Arcadia/IdentityServerHost/obj/Debug/net8.0/IdentityServerHost.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServerHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dd1cdc116f338ead85a525ec159fad915c9ebef")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServerHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServerHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
