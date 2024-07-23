df97117815e97083b7f808b54dd3127de60087b7
LaconisFaucheux <nicolas.frotey@gmail.com>
2024-07-23 22:18:58 +0200

    commit dodo

--- API_Arcadia/obj/Debug/net8.0/API_Arcadia.AssemblyInfo.cs
+++ API_Arcadia/obj/Debug/net8.0/API_Arcadia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a5adbb7e607cc8185c22ed0b53bf0cfb6d043a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16c6d89204634b5fdc6e086e4a37ae76820282cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Arcadia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
